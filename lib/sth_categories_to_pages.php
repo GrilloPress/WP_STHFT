3f1e787935a11bb58356d57140c7da0734802dc2
GrilloPress <grillopress@gmail.com>
2016-07-31 15:56:00 +0000

    add function exists check to categories to pages function

--- lib/sth_categories_to_pages.php
+++ lib/sth_categories_to_pages.php
@@ -8,24 +8,32 @@
  * @package sth
  */
 
-function add_taxonomies_to_pages() {
- register_taxonomy_for_object_type( 'post_tag', 'page' );
- register_taxonomy_for_object_type( 'category', 'page' );
- }
+if ( ! function_exists( 'add_taxonomies_to_pages' ) ) :
+
+  function add_taxonomies_to_pages() {
+    
+    register_taxonomy_for_object_type( 'post_tag', 'page' );
+    register_taxonomy_for_object_type( 'category', 'page' );
+   
+  }
+
+  add_action( 'init', 'add_taxonomies_to_pages' );
 
 
-add_action( 'init', 'add_taxonomies_to_pages' );
- if ( ! is_admin() ) {
- add_action( 'pre_get_posts', 'category_and_tag_archives' );
- 
- }
 
-function category_and_tag_archives( $wp_query ) {
-$my_post_array = array('post','page');
- 
- if ( $wp_query->get( 'category_name' ) || $wp_query->get( 'cat' ) )
- $wp_query->set( 'post_type', $my_post_array );
- 
- if ( $wp_query->get( 'tag' ) )
- $wp_query->set( 'post_type', $my_post_array );
-}
+   if ( ! is_admin() ) {
+   add_action( 'pre_get_posts', 'category_and_tag_archives' );
+
+   }
+
+  function category_and_tag_archives( $wp_query ) {
+  $my_post_array = array('post','page');
+
+   if ( $wp_query->get( 'category_name' ) || $wp_query->get( 'cat' ) )
+   $wp_query->set( 'post_type', $my_post_array );
+
+   if ( $wp_query->get( 'tag' ) )
+   $wp_query->set( 'post_type', $my_post_array );
+  }
+
+endif;
