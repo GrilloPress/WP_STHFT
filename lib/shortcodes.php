0a1fffb9f4935bff0664a7e21a79c642b2bf7ac6
GrilloPress <grillopress@gmail.com>
2016-08-02 22:50:19 +0000

    add complete bootstrap media shortcodes

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -3,17 +3,40 @@
 // Shortcodes
 //
 
+// Bootstrap media shortcodes
+// 
+// http://getbootstrap.com/components/#media
+
 // [media class="class_you_want_to_add"] other shortcodes in here [/media]
 function boostrap_media( $atts, $content = null ) {
     $a = shortcode_atts( array(
-        'class' => '',
+      'class' => '',
     ), $atts );
 
-    return "<div class='media " . $a['class'] . "'>" . do_shortcode($content) ."</div>";
+    return "<div class='media " . esc_attr($a['class']) . "'>" . do_shortcode( wp_kses_post($content) ) ."</div>";
 }
 add_shortcode( 'media', 'boostrap_media' );
 
-// [media-object][/media-object]
+// [media-object class="whatever" alignment="right" valignment="middle"][/media-object]
+function boostrap_media_object( $atts, $content = null ) {
+    $a = shortcode_atts( array(
+      'class' => '',
+      'alignment' => 'left',
+      'valignment' => '',
+    ), $atts );
 
+    return "<div class='media-" . esc_attr($a['alignment']) . "" . esc_attr($a['valignment']) .  " " . esc_attr($a['class']) . "'>" . wp_kses_post($content) ."</div>";
+}
+add_shortcode( 'media-object', 'boostrap_media_object' );
 
-// [media-body][/media-body]
+// [media-body class="call_you_want_extra" title="this will be a header four media"][/media-body]
+
+function boostrap_media_body( $atts, $content = null ) {
+    $a = shortcode_atts( array(
+      'class' => '',
+      'title' => '',
+    ), $atts );
+
+    return "<div class='media-body " . esc_attr($a['class']) . "'>" . "<h4 class='media-heading'>" . esc_html( $a['title'] ) . "</h4>" . wp_kses_post($content) ."</div>";
+}
+add_shortcode( 'media-body', 'boostrap_media_body' );
