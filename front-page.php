68c6a8424244fd67e7511ab8401446cb697e752b
GrilloPress <grillopress@gmail.com>
2016-08-13 21:34:30 +0000

    add custom fields UI fields to front page jumbotron

--- front-page.php
+++ front-page.php
@@ -11,10 +11,27 @@
  * 
  * 
  * 
- * Template Name: Homepage
+ * 
  */
 
 get_header(); ?>
+
+<div class="jumbotron" style="background-image: url('<?php echo CFS()->get( 'jumbotron_img' );?>')">
+   <div class="container">
+     
+     <h1><?php echo CFS()->get( 'jumbotron_title' );?></h1>
+     
+     <p>
+       <?php echo CFS()->get( 'jumbotron_subtitle' );?>
+     </p>
+     
+     <p>
+       <?php $jbo_link = CFS()->get( 'jumbotron_btn' );?>
+       <a class="btn btn-primary btn-lg" href="<?php echo $jbo_link["url"];?>" title="<?php echo $jbo_link["text"];?>" target="<?php echo $jbo_link["target"];?>" role="button"><?php echo $jbo_link["text"];?></a>
+     </p>
+  </div>
+
+</div>
 
 	 <div id="primary" class="container">
     <div class="row">
