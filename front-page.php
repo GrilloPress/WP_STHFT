31d38fe77df54d51fe2e25561c1521220ceb8838
GrilloPress <grillopress@gmail.com>
2016-08-15 06:48:28 +0000

    add initial feature block to front-page.php

--- front-page.php
+++ front-page.php
@@ -16,7 +16,7 @@
 
 get_header(); ?>
 
-<div class="jumbotron" style="background-image: url('<?php echo CFS()->get( 'jumbotron_img' );?>')">
+<section class="jumbotron" style="background-image: url('<?php echo CFS()->get( 'jumbotron_img' );?>')">
    <div class="container">
      
      <h1><?php echo CFS()->get( 'jumbotron_title' );?></h1>
@@ -30,8 +30,38 @@ get_header(); ?>
        <a class="btn btn-primary btn-lg" href="<?php echo $jbo_link["url"];?>" title="<?php echo $jbo_link["text"];?>" target="<?php echo $jbo_link["target"];?>" role="button"><?php echo $jbo_link["text"];?></a>
      </p>
   </div>
+</section>
 
-</div>
+<section class="page-feature-container gray">
+  <div class="container">
+    <div class="row">
+      
+      <?php
+      $featured_pages = array("one", "two", "three");
+      foreach ($featured_pages as $fp) { ;?>
+      
+      
+      <div class="col-md-4">
+        <div class="page-feature-block">
+          
+          <?php echo CFS()->get( 'feature_image_' . $fp );?>
+         
+          <h4><?php echo CFS()->get('feature_title_' . $fp); ?></h4>
+
+          <p><?php echo CFS()->get('feature_body_' . $fp); ?></p>
+
+          <?php $fb_link = CFS()->get( 'feature_image_' . $fp );?>
+          <p>
+            <a class="btn btn-warning" href="<?php echo $fb_link["url"];?>" title="<?php echo $fb_link["text"];?>" target="<?php echo $fb_link["target"];?>" role="button"><?php echo $fb_link["text"];?></a>
+          </p>
+          
+        </div>
+      </div>
+      
+      <?php } reset($featured_pages);?>
+    </div>
+  </div>
+</section>
 
 	 <div id="primary" class="container">
     <div class="row">
