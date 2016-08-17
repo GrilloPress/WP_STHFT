a8a09bcfbdd37abc3bb145e776f54250e6e793cb
GrilloPress <grillopress@gmail.com>
2016-08-17 17:21:22 +0000

    revert block section to bottom

--- front-page.php
+++ front-page.php
@@ -63,49 +63,6 @@ get_header(); ?>
   </div>
 </section>
 
-<section class="page-marketing-four-columns-container">
-  <div class="container">
-    <div class="row">
-      
-      <h2></h2>
-      
-      <?php
-      $marketing_blocks = array("one", "two", "three", "four");
-      foreach ($marketing_blocks as $mb) { ;?>
-      
-      <div class="col-md-3 col-sm-6">
-              
-            <?php $marketing_image_id = CFS()->get( 'marketing_image_' . $mb );
-            echo wp_get_attachment_image( $marketing_image_id, array('84', '84'), "", array( "class" => "img-marketing" ) );?>
-            
-            <h4 class="media-heading"><?php echo CFS()->get('marketing_title_' . $mb); ?></h4>
-            <?php echo CFS()->get('marketing_body_' . $mb); ?>
-
-      </div>
-      
-      <?php } reset($marketing_blocks);?>
-    </div>
-    
-    <div class="row">
-      <?php
-      $marketing_blocks = array("five", "six", "seven", "eight");
-      foreach ($marketing_blocks as $mb) { ;?>
-      
-      <div class="col-md-3 col-sm-6">
-
-
-            <?php $marketing_image_id = CFS()->get( 'marketing_image_' . $mb );
-            echo wp_get_attachment_image( $marketing_image_id, array('84', '84'), "", array( "class" => "img-marketing" ) );?>
-        
-            <h4 class="media-heading"><?php echo CFS()->get('marketing_title_' . $mb); ?></h4>
-            <?php echo CFS()->get('marketing_body_' . $mb); ?>
-
-      </div>
-      <?php } reset($marketing_blocks);?>
-    </div>
-  </div>
-</section>
-
 <section id="primary" class="page-service-marketing-container">
   <div class="container">
     <div class="row">
@@ -156,6 +113,49 @@ get_header(); ?>
     </div><!-- #primary -->
     
 
+  </div>
+</section>
+
+<section class="page-marketing-four-columns-container">
+  <div class="container">
+    <div class="row">
+      
+      <h2></h2>
+      
+      <?php
+      $marketing_blocks = array("one", "two", "three", "four");
+      foreach ($marketing_blocks as $mb) { ;?>
+      
+      <div class="col-md-3 col-sm-6">
+              
+            <?php $marketing_image_id = CFS()->get( 'marketing_image_' . $mb );
+            echo wp_get_attachment_image( $marketing_image_id, array('300', '9999'), "", array( "class" => "img-marketing" ) );?>
+            
+            <h4 class="media-heading"><?php echo CFS()->get('marketing_title_' . $mb); ?></h4>
+            <?php echo CFS()->get('marketing_body_' . $mb); ?>
+
+      </div>
+      
+      <?php } reset($marketing_blocks);?>
+    </div>
+    
+    <div class="row">
+      <?php
+      $marketing_blocks = array("five", "six", "seven", "eight");
+      foreach ($marketing_blocks as $mb) { ;?>
+      
+      <div class="col-md-3 col-sm-6">
+
+
+            <?php $marketing_image_id = CFS()->get( 'marketing_image_' . $mb );
+            echo wp_get_attachment_image( $marketing_image_id, array('300', '9999'), "", array( "class" => "img-marketing" ) );?>
+        
+            <h4 class="media-heading"><?php echo CFS()->get('marketing_title_' . $mb); ?></h4>
+            <?php echo CFS()->get('marketing_body_' . $mb); ?>
+
+      </div>
+      <?php } reset($marketing_blocks);?>
+    </div>
   </div>
 </section>
 
